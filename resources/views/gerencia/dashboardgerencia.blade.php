8503c3ee211f3cd2f58791c76d060e8a241c6f59
EliasE83 <mayorelias83@gmail.com>
2023-11-02 17:06:55 -0600

    commit inicial

---
+++ resources/views/gerencia/dashboardgerencia.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.plantillagerencia')
+
+@section('title', 'Gerencia')
+
+@section('contenido')
+
+<div class="container mt-5 col-md-10" style="background: #043D3C;">
+
+    
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GerenciaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,12 @@ require __DIR__.'/auth.php';
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+/* Rutas para módulo gerencia */
+
+Route::controller(GerenciaController::class)->group(function (){
+    Route::get('/gerencia', 'index')->name('indexgerencia');
+});
+
+
