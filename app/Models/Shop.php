7d7fa25052afafe03ab1214f54d8b03e273c6610
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2023-12-03 12:37:30 -0600

    merge de diego

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+
+    
+    // ***************************************************
+    // Relationship
+    // ***************************************************
+
+    /**
+     * Uniformes asociados a empleados
+     **/
+    public function uniforms()
+    {
+        return $this->belongsToMany(Uniform::class, 'employee_uniforms')
+                    ->withPivot('application_date', 'delivery_date', 'status', 'description', 'amount', 'state', 'document_support', 'employee_id','uniform_id','id');
+    }
+
 }
