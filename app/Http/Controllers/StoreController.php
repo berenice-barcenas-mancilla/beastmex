03db9b57ecb619827898e709ecb9294d99fcbebd
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2023-12-04 07:21:10 -0600

    MERGE

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -27,7 +27,7 @@ class StoreController extends Controller
         });
     }
 
-    /**
+        /**
      * Display a listing of the resource.
      */
     public function index( Request $request  )
@@ -50,21 +50,21 @@ class StoreController extends Controller
         if (!Gate::allows('system.store.create')) {
             abort(403, "No estas autorizado para registrar información");
         }
-        $validatedData = $request->validate([
-            'txtName' => 'required|string',
-            'txtSerialNumber' => 'required|integer',
-            'txtStock' => 'required|integer',
-            'brand' => 'required',
-            'txtPurchaseCost' => 'required|numeric',
-            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
-
-        if (!$request->hasFile('photo') || !$request->file('photo')->isValid()) {
-            return redirect('/store')->with('Error', 'Por favor, seleccione un archivo de imagen válido.');
-        }
-
-        $imageName = $request->txtSerialNumber . '_' . time() . '.' . $request->file('photo')->extension();
-        $request->file('photo')->move(public_path('images'), $imageName);
+            $validatedData = $request->validate([
+                'txtName' => 'required|string',
+                'txtSerialNumber' => 'required|integer',
+                'txtStock' => 'required|integer',
+                'brand' => 'required',
+                'txtPurchaseCost' => 'required|numeric',
+                'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+            
+            if (!$request->hasFile('photo') || !$request->file('photo')->isValid()) {
+                return redirect('/store')->with('Error', 'Por favor, seleccione un archivo de imagen válido.');
+            }
+            
+            $imageName = $request->txtSerialNumber . '_' . time() . '.' . $request->file('photo')->extension();
+            $request->file('photo')->move(public_path('images'), $imageName);
 
         $addProducto = new Store();
         $addProducto->nombre = $request->input('txtName');
@@ -73,14 +73,14 @@ class StoreController extends Controller
         $addProducto->stock = $request->input('txtStock');
         $addProducto->costoCompra = $request->input('txtPurchaseCost');
         $addProducto->precioVenta = $request->input('txtPurchaseCost') + ($request->input('txtPurchaseCost') * 0.55);
-        $addProducto->fechaIngreso = $request->input('txtEntryDate');
+        $addProducto->fechaIngreso = $request->input('txtEntryDate');       
         $addProducto->foto = $imageName;
         $addProducto->estatus = $request->input('status');
         $addProducto->save();
-        return redirect('/store')->with('Exito', 'El producto ' . $addProducto->nombre . ' se ha registrado con éxito');
+        return redirect('/store')->with('Exito',  'El producto ' . $addProducto->nombre . ' se ha registrado con éxito');
     }
-
-
+   
+    
     /**
      * Update the specified resource in storage.
      */
@@ -124,10 +124,14 @@ class StoreController extends Controller
     
         $updateProducto->estatus = $request->input('status');
         $updateProducto->update();
+    
         return redirect('/store')->with('Exito', 'El producto ' . $updateProducto->nombre . ' se ha actualizado con éxito');
-
     }
+    
 
+
+
+    /**
      * change the status from storage.
      */
     public function updateStatus(Request $request, $id)
@@ -138,20 +142,18 @@ class StoreController extends Controller
 
         $statusProducto = Store::findOrFail($id);
         $statusProducto->estatus = $statusProducto->estatus == 0 ? 1 : 0;
-
+        
         $statusProducto->update();
         return redirect('/store')->with('Exito', 'El producto ' . $statusProducto->nombre . ' se ha actualizado con éxito');
     }
-
-
-    public function search(Request $request)
-    {
-        $query = $request->input('query');
-        $results = Store::where('nombre', 'like', '%' . $query . '%')
-            ->orWhere('noDeSerie', 'like', '%' . $query . '%')
-            ->get();
-        return response()->json($results);
-    }
+    // public function search(Request $request)
+    // {
+    //     $query = $request->input('query');
+    //     $results = Store::where('nombre', 'like', '%' . $query . '%')
+    //         ->orWhere('noDeSerie', 'like', '%' . $query . '%')
+    //         ->get();
+    //     return response()->json($results);
+    // }
 
 
     /***********************************
@@ -181,30 +183,6 @@ class StoreController extends Controller
 
         }
     }
-
-
-    public function getInfo(Store  $store)
-    {
-        // Verifica si el compras existe
-        if (!isset($store->id)){
-            return response()->json(['exito'=>false]);   
-        } else {
-            // Retorna la información del compras en formato JSON
-            return response()->json(['exito'=>true, 'shop' => $store]);   
-
-        }
-
-
-      public function search(Request $request)
-  {
-      $query = $request->input('query');
-      $results = Store::where('nombre', 'like', '%' . $query . '%')
-          ->orWhere('noDeSerie', 'like', '%' . $query . '%')
-          ->get();
-      return response()->json($results);
-  }
-
-
 }
 
 
